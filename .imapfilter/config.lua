5df54fc8cdc72ad97918fd8270830641da820dde
Victor Engmark <victor.engmark@gmail.com>
2011-07-12 09:37:48 +0200

    Function to search To, Cc and Bcc address fields together

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -10,4 +10,10 @@ options.create = true
 -- for debugging purposes
 options.info = true
 
+recipient_mail = function (mailbox, address)
+    return mailbox:contain_to(address) +
+           mailbox:contain_bcc(address) +
+           mailbox:contain_cc(address)
+end
+
 dofile(os.getenv("HOME") .. "/.imapfilter/config_local.lua")
