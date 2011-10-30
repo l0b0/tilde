89c8eb22fa5108c94d0ae1b8261cbc610083999d
Victor Engmark <victor.engmark@gmail.com>
2011-10-30 13:39:51 +0100

    Avoid crash when changing JRE

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -15,7 +15,7 @@ apt-get install -y keepassx # keepass2 is not ready for production
 apt-get install markdown
 apt-get install openssh-server
 apt-get install -y sun-java6-jre
-update-java-alternatives -s java-6-sun
+update-java-alternatives --jre -s java-6-sun
 apt-get install sun-java6-fonts
 apt-get install -y unrar
 apt-get install wine
