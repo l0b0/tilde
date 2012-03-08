28ea142a239e1ce5ae572c502b723a4187569d87
Victor Engmark <victor.engmark@gmail.com>
2012-03-08 13:47:48 +0100

    Documentation update
    Based on <http://unix.stackexchange.com/a/33762/3645>.

--- examples/safe-find.sh
+++ examples/safe-find.sh
@@ -1,42 +1,57 @@
 #!/usr/bin/env bash
-# Filenames can contain *any* character except only null (\0) and slash (/);
-# here's some general rules to handle them:
 #
-# $'...' can be used to create human readable strings with escape sequences.
+# *Filenames* can contain *any* character except only NUL (`$'\0'`) and slash
+# (`/`); therefore, *paths* can contain any character except NUL. Here are some
+# guidelines to handle them:
 #
-# ' -- ' in commands is necessary to separate arguments from filenames, since
-# filenames can start with '--', and would therefore be handled as parameters.
-# To handle parameters properly (like GNU tools) use `getopt`.
+# Strings containing variables should always be double-quoted to avoid
+# accidental word splitting. The only exception from this is the
+# `[[ "var" =~ regex ]]` construct, where the `regex` string must not be quoted.
 #
-# `find` doesn't support this syntax, so we use `readlink` to get an absolute
-# path which by definition starts with slash.
+# `$'...'` can be used to create human readable strings with escape sequences.
+# The complicated file name below is used to test that the code is able to
+# handle several common filename issues.
 #
-# The "$()" construct strips trailing newlines, so we have to add a different
-# character and then strip it outside the "$()" construct.
+# ` -- ` in commands is necessary to separate arguments from filenames, since
+# filenames can start with `--`, and would therefore be handled as parameters.
+# To handle parameters properly (like most *nix tools) use GNU `getopt` or the
+# Bash built-in (but more limited) `getopts`.
 #
-# `IFS=` is necessary to avoid that any characters in IFS are stripped from
-# the start and end of $REPLY.
+# GNU `find` doesn't support the ` -- ` separator. This is only a problem if the
+# search path is a relative path which *doesn't* start with `./` (since absolute
+# paths by definition start with a slash, they are not a problem). If the
+# application should support *any* filename, such paths have to be either
+# converted into absolute paths by using readlink or by adding `./` to the start
+# of relative paths.
 #
-# '-r' avoids interpreting backslash in filenames specially.
+# Variables inside `$()` have to be quoted just like outside this construct. No
+# additional quoting or escaping is necessary.
 #
-# '-d ""' splits filenames by the null character.
+# `read` usually strips characters in `$IFS` from the start and end of the
+# resulting variable. Therefore `$IFS` must be emptied when reading a line.
 #
-# '-print0' separates find output by null characters.
+# `read`'s `-r` argument avoids interpreting backslash in filenames specially.
 #
-# Variables inside '$()' have to be quoted just like outside this construct.
+# `read`'s `-d ''` argument splits the input by the null character.
 #
-# Use process substitution with "<(" instead of pipes to avoid broken pipes.
+# The `$()` and <code>\`\`</code> constructs *remove trailing newlines*. Getting
+# the original string printed by the commands inside it is a simple matter of
+# adding a single printable character after the last command and then remove it
+# on the next line without using another `$()`.
 #
-# Use file descriptor 9 for data storage instead of standard input to avoid
-# greedy commands like `cat` eating all of it.
+# Use process substitution with `<(` instead of pipes to avoid broken pipes.
+#
+# `find`'s `-print0` argument separates the output by null characters.
+#
+# Use a file descriptor between 3 and 9 for data passing instead of standard
+# input to avoid greedy commands like `cat` or `ssh` slurping all of it.
 #
 # As with all software ever, this comes with no warranty whatsoever. However,
 # the code has been subjected to public scrutiny (and a bounty for a week)
 # without anyone reporting an error
 # <http://codereview.stackexchange.com/questions/1343/can-you-break-this-code>.
 # If you find some way to break this code as described after the jump, please
-# send a message either there or at the GitHub issue tracker
-# <https://github.com/l0b0/tilde/issues>.
+# report it at <https://github.com/l0b0/tilde/issues>.
 
 set -o errexit
 set -o nounset
