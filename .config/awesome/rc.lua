a3e280c0e1d2807c0a8111a231b082e71fe32c9f
Victor Engmark <victor.engmark@gmail.com>
2012-09-02 20:12:35 +0200

    Show Firefox Flash fullscreen on top

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -347,6 +347,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { instance = "plugin-container" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
