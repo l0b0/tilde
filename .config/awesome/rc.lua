6bdcbe67b042d04f889790f938c7ec8d5562b61f
Victor Engmark <victor.engmark@gmail.com>
2013-02-05 17:39:39 +0100

    Tag names and layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -71,10 +71,13 @@ layouts =
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+    names = {"shell", "edit", "doc", "www", "vcs", "db", "toread", "chat", "fun"},
+    layouts = {layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2], layouts[2]}
+}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag(tags.names, s, tags.layouts)
 end
 -- }}}
 
