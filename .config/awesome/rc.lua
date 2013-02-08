072467f187b72e5f1e3330c957cb171a961b548f
Victor Engmark <victor.engmark@gmail.com>
2013-02-08 14:33:44 +0100

    Float yet another type of Flash window

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -354,6 +354,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Luakit" },
       properties = { floating = true } },
+    { rule = { instance = "exe" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
