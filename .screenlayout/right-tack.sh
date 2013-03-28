7437a4ab12b613f1fa19cdfe8de1cb814fba2383
Victor Engmark <victor.engmark@gmail.com>
2013-03-28 13:46:13 +0100

    Dynamic monitor resolutions assignment
    Parses `xrandr` information into a usable format. If the monitors are flipped, all that's necessary is to swap the connectors.

--- .screenlayout/right-tack.sh
+++ .screenlayout/right-tack.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
-width=1920
-height=1200
-offset=$(( ( ${width} - ${height} ) / 2 ))
-readonly width height offset
+. "`dirname -- "$0"`/library.sh"
+expected_monitors=2
+if [ "${monitor_count:-0}" -ne "$expected_monitors" ]
+then
+    echo "$0: Expected ${expected_monitors} monitors; found ${monitor_count:-0}." >&2
+    exit 1
+fi
+
+monitor1=`echo "$monitor_resolutions" | head -1`
+monitor2=`echo "$monitor_resolutions" | tail -1`
+
+read monitor1_name monitor1_width monitor1_height <<EOF
+$monitor1
+EOF
+
+read monitor2_name monitor2_width monitor2_height <<EOF
+$monitor2
+EOF
+
+offset=$(( ( ${monitor1_width} - ${monitor2_height} ) / 2 ))
 
 xrandr \
-    --output DVI-I-1 --mode ${width}x${height} --pos 0x0 --rotate left \
-    --output DP-1 --mode ${width}x${height} --pos ${height}x${offset} --rotate normal
+    --output "$monitor1_name" \
+        --mode ${monitor1_width}x${monitor1_height} \
+        --pos 0x0 \
+        --rotate left \
+    --output "$monitor2_name" \
+        --mode ${monitor2_width}x${monitor2_height} \
+        --pos ${monitor1_height}x${offset} \
+        --rotate normal
--- .screenlayout/side-by-side.sh
+++ .screenlayout/side-by-side.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
-width=1920
-height=1200
-readonly width height
+. "`dirname -- "$0"`/library.sh"
+expected_monitors=2
+if [ "${monitor_count:-0}" -ne "$expected_monitors" ]
+then
+    echo "$0: Expected ${expected_monitors} monitors; found ${monitor_count:-0}." >&2
+    exit 1
+fi
+
+monitor1=`echo "$monitor_resolutions" | head -1`
+monitor2=`echo "$monitor_resolutions" | tail -1`
+
+read monitor1_name monitor1_width monitor1_height <<EOF
+$monitor1
+EOF
+
+read monitor2_name monitor2_width monitor2_height <<EOF
+$monitor2
+EOF
 
 xrandr \
-    --output DVI-0 --mode ${width}x${height} \
-    --output DVI-1 --mode ${width}x${height} --right-of DVI-0
+    --output "$monitor1_name" \
+        --mode ${monitor1_width}x${monitor1_height} \
+    --output "$monitor2_name" \
+        --mode ${monitor2_width}x${monitor2_height} \
+        --right-of "$monitor1_name"
