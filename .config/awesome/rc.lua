251f5eeffe6a27b5e9d4bbe3324b47c54bedfca6
Victor Engmark <victor.engmark@gmail.com>
2013-04-26 19:13:45 +0200

    Enable swapping between clients like with Alt(-Shift)-Tab

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -266,7 +266,14 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
-            awful.client.focus.history.previous()
+            awful.client.focus.byidx(1)
+            if client.focus then
+                client.focus:raise()
+            end
+        end),
+    awful.key({ modkey, "Shift" }, "Tab",
+        function()
+            awful.client.focus.byidx(-1)
             if client.focus then
                 client.focus:raise()
             end
