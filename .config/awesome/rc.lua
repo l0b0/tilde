1e00f9b9bbf0e1bb4ce1984b17992d64357c06c6
Victor Engmark <victor.engmark@gmail.com>
2013-06-17 23:53:13 +0200

    Show ISO datetime

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -144,7 +144,8 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+mytextclock = awful.widget.textclock({ align = "right" },
+                                     " %a %Y-%m-%d %H:%M ")
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
