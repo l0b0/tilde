eb5e9874125df3b06122797fefb2f99546049467
Victor Engmark <victor.engmark@gmail.com>
2013-08-27 21:16:39 +0100

    Default settings

---
+++ .config/calibre/global.py
@@ -0,0 +1,90 @@
+# calibre wide preferences
+
+### Begin group: DEFAULT
+ 
+# database path
+# Path to the database in which books are stored
+database_path = '/home/username/library1.db'
+ 
+# filename pattern
+# Pattern to guess metadata from filenames
+filename_pattern = u'(?P<title>.+) - (?P<author>[^_]+)'
+ 
+# isbndb com key
+# Access key for isbndb.com
+isbndb_com_key = ''
+ 
+# network timeout
+# Default timeout for network operations (seconds)
+network_timeout = 5
+ 
+# library path
+# Path to directory in which your library of books is stored
+library_path = '/home/username/Calibre Library'
+ 
+# language
+# The language in which to display the user interface
+language = 'en'
+ 
+# output format
+# The default output format for ebook conversions.
+output_format = 'EPUB'
+ 
+# input format order
+# Ordered list of formats to prefer for input.
+input_format_order = cPickle.loads('\x80\x02]q\x01(U\x04EPUBq\x02U\x04AZW3q\x03U\x04MOBIq\x04U\x03LITq\x05U\x03PRCq\x06U\x03FB2q\x07U\x04HTMLq\x08U\x03HTMq\tU\x04XHTMq\nU\x05SHTMLq\x0bU\x05XHTMLq\x0cU\x03ZIPq\rU\x03ODTq\x0eU\x03RTFq\x0fU\x03PDFq\x10U\x03TXTq\x11e.')
+ 
+# read file metadata
+# Read metadata from files
+read_file_metadata = True
+ 
+# worker process priority
+# The priority of worker processes. A higher priority means they run faster and consume more resources. Most tasks like conversion/news download/adding books/etc. are affected by this setting.
+worker_process_priority = 'normal'
+ 
+# swap author names
+# Swap author first and last names when reading metadata
+swap_author_names = False
+ 
+# add formats to existing
+# Add new formats to existing book records
+add_formats_to_existing = False
+ 
+# installation uuid
+# Installation UUID
+installation_uuid = '33ac5ee3-f090-4bf9-a6e0-322a803d5c7f'
+ 
+# new book tags
+# Tags to apply to books added to the library
+new_book_tags = cPickle.loads('\x80\x02]q\x01.')
+ 
+# saved searches
+# List of named saved searches
+saved_searches = cPickle.loads('\x80\x02}q\x01.')
+ 
+# user categories
+# User-created tag browser categories
+user_categories = cPickle.loads('\x80\x02}q\x01.')
+ 
+# manage device metadata
+# How and when calibre updates metadata on the device.
+manage_device_metadata = 'manual'
+ 
+# limit search columns
+# When searching for text without using lookup prefixes, as for example, Red instead of title:Red, limit the columns searched to those named below.
+limit_search_columns = False
+ 
+# limit search columns to
+# Choose columns to be searched when not using prefixes, as for example, when searching for Red instead of title:Red. Enter a list of search/lookup names separated by commas. Only takes effect if you set the option to limit search columns above.
+limit_search_columns_to = cPickle.loads('\x80\x02]q\x01(U\x05titleq\x02U\x07authorsq\x03U\x04tagsq\x04U\x06seriesq\x05U\tpublisherq\x06e.')
+ 
+# use primary find in search
+# Characters typed in the search box will match their accented versions, based on the language you have chosen for the calibre interface. For example, in  English, searching for n will match ñ and n, but if your language is Spanish it will only match n. Note that this is much slower than a simple search on very large libraries.
+use_primary_find_in_search = True
+ 
+# migrated
+# For Internal use. Don't modify.
+migrated = False
+ 
+
+
---
+++ .config/calibre/gui.py
@@ -0,0 +1,174 @@
+# preferences for the calibre GUI
+
+### Begin group: DEFAULT
+ 
+# send to storage card by default
+# Send file to storage card instead of main memory by default
+send_to_storage_card_by_default = False
+ 
+# confirm delete
+# Confirm before deleting
+confirm_delete = False
+ 
+# main window geometry
+# Main window geometry
+main_window_geometry = cPickle.loads('\x80\x02csip\n_unpickle_type\nq\x01U\x0cPyQt4.QtCoreq\x02U\nQByteArrayU.\x01\xd9\xd0\xcb\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x13\x00\x00\x05U\x00\x00\x02\xff\x00\x00\x00\x01\x00\x00\x00\x14\x00\x00\x05T\x00\x00\x02\xfe\x00\x00\x00\x00\x00\x00\x85\x87Rq\x03.')
+ 
+# new version notification
+# Notify when a new version is available
+new_version_notification = True
+ 
+# use roman numerals for series number
+# Use Roman numerals for series number
+use_roman_numerals_for_series_number = True
+ 
+# sort tags by
+# Sort tags list by name, popularity, or rating
+sort_tags_by = 'name'
+ 
+# match tags type
+# Match tags by any or all.
+match_tags_type = 'any'
+ 
+# cover flow queue length
+# Number of covers to show in the cover browsing mode
+cover_flow_queue_length = 6
+ 
+# LRF conversion defaults
+# Defaults for conversion to LRF
+LRF_conversion_defaults = cPickle.loads('\x80\x02]q\x01.')
+ 
+# LRF ebook viewer options
+# Options for the LRF ebook viewer
+LRF_ebook_viewer_options = None
+ 
+# internally viewed formats
+# Formats that are viewed using the internal viewer
+internally_viewed_formats = cPickle.loads('\x80\x02]q\x01(U\x03LRFq\x02U\x04EPUBq\x03U\x03LITq\x04U\x04MOBIq\x05U\x03PRCq\x06U\x04POBIq\x07U\x03AZWq\x08U\x04AZW3q\tU\x04HTMLq\nU\x03FB2q\x0bU\x03PDBq\x0cU\x02RBq\rU\x03SNBq\x0eU\x05HTMLZq\x0fe.')
+ 
+# column map
+# Columns to be displayed in the book list
+column_map = cPickle.loads('\x80\x02]q\x01(U\x05titleq\x02U\x08ondeviceq\x03U\x07authorsq\x04U\x04sizeq\x05U\ttimestampq\x06U\x06ratingq\x07U\tpublisherq\x08U\x04tagsq\tU\x06seriesq\nU\x07pubdateq\x0be.')
+ 
+# autolaunch server
+# Automatically launch content server on application startup
+autolaunch_server = False
+ 
+# oldest news
+# Oldest news kept in database
+oldest_news = 60
+ 
+# systray icon
+# Show system tray icon
+systray_icon = False
+ 
+# upload news to device
+# Upload downloaded news to device
+upload_news_to_device = True
+ 
+# delete news from library on upload
+# Delete news books from library after uploading to device
+delete_news_from_library_on_upload = False
+ 
+# separate cover flow
+# Show the cover flow in a separate window instead of in the main calibre window
+separate_cover_flow = False
+ 
+# disable tray notification
+# Disable notifications from the system tray icon
+disable_tray_notification = False
+ 
+# default send to device action
+# Default action to perform when send to device button is clicked
+default_send_to_device_action = 'DeviceAction:main::False:False'
+ 
+# asked library thing password
+# Asked library thing password at least once.
+asked_library_thing_password = False
+ 
+# search as you type
+# Start searching as you type. If this is disabled then search will only take place when the Enter or Return key is pressed.
+search_as_you_type = False
+ 
+# highlight search matches
+# When searching, show all books with search results highlighted instead of showing only the matches. You can use the N or F3 keys to go to the next match.
+highlight_search_matches = False
+ 
+# save to disk template history
+# Previously used Save to Disk templates
+save_to_disk_template_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# send to device template history
+# Previously used Send to Device templates
+send_to_device_template_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# main search history
+# Search history for the main GUI
+main_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# viewer search history
+# Search history for the ebook viewer
+viewer_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# lrf viewer search history
+# Search history for the LRF viewer
+lrf_viewer_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# scheduler search history
+# Search history for the recipe scheduler
+scheduler_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# plugin search history
+# Search history for the plugin preferences
+plugin_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# shortcuts search history
+# Search history for the keyboard preferences
+shortcuts_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# jobs search history
+# Search history for the keyboard preferences
+jobs_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# tweaks search history
+# Search history for tweaks
+tweaks_search_history = cPickle.loads('\x80\x02]q\x01.')
+ 
+# worker limit
+# Maximum number of simultaneous conversion/news download jobs. This number is twice the actual value for historical reasons.
+worker_limit = 6
+ 
+# get social metadata
+# Download social metadata (tags/rating/etc.)
+get_social_metadata = True
+ 
+# overwrite author title metadata
+# Overwrite author and title with new metadata
+overwrite_author_title_metadata = True
+ 
+# auto download cover
+# Automatically download the cover, if available
+auto_download_cover = False
+ 
+# enforce cpu limit
+# Limit max simultaneous jobs to number of CPUs
+enforce_cpu_limit = True
+ 
+# gui layout
+# The layout of the user interface. Wide has the book details panel on the right and narrow has it at the bottom.
+gui_layout = 'wide'
+ 
+# show avg rating
+# Show the average rating per item indication in the tag browser
+show_avg_rating = True
+ 
+# disable animations
+# Disable UI animations
+disable_animations = False
+ 
+# tag browser hidden categories
+# tag browser categories not to display
+tag_browser_hidden_categories = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]\x85Rq\x02.')
+ 
+
+
