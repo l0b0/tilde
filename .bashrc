l0b0/tilde

commit fef5a4701fcece77895ca9ba29129ab73a8f8207

Remove references to Subversion
Moved PS1 script to <https://github.com/l0b0/svn_ps1>.

.bashrc
@@ -44,10 +44,6 @@ then
     source /etc/bash_completion
     set -o nounset
 fi
-if [[ -r ~/dev/tilde/scripts/__svn_ps1.sh ]]
-then
-    source ~/dev/tilde/scripts/__svn_ps1.sh
-fi
 
 color_enabled() {
     local -i colors=$(tput colors 2>/dev/null)
@@ -135,24 +131,6 @@ then
     export GIT_PS1_SHOWSTASHSTATE=1
     export GIT_PS1_SHOWUPSTREAM="auto"
 fi
-
-# Subversion branch
-svn_prompt() {
-    if ! type -t __svn_ps1 >/dev/null
-    then
-        return
-    fi
-
-    # Warn if you're not in the top directory of the checkout
-    if [[ -d ../.svn ]]
-    then
-        __svn_ps1 ' ('${BOLD_FORMAT}${WARNING_FORMAT}%s${RESET_FORMAT}')'
-    else
-        __svn_ps1 ' (%s)'
-    fi
-}
-
-PS1="$PS1"'$(svn_prompt)\n\$ '
 
 # Default editor
 export GIT_EDITOR='vim'

.bash_history
@@ -1158,6 +1158,7 @@ mkgithub ~/dev/Smooth-CoffeeScript
 mkgithub ~/dev/spark 
 mkgithub ~/dev/Standardy 
 mkgithub ~/dev/subsurface 
+mkgithub ~/dev/svn_ps1 
 mkgithub ~/dev/teaching_C 
 mkgithub ~/dev/tilde 
 mkgithub ~/dev/trim 

.subversion/.gitignore
@@ -1,3 +1,0 @@
-auth/
-README.txt
-servers

.subversion/config
@@ -1,197 +1,0 @@
-### This file configures various client-side behaviors.
-###
-### The commented-out examples below are intended to demonstrate
-### how to use this file.
-
-### Section for authentication and authorization customizations.
-[auth]
-### Set password stores used by Subversion. They should be
-### delimited by spaces or commas. The order of values determines
-### the order in which password stores are used.
-### Valid password stores:
-###   gnome-keyring        (Unix-like systems)
-###   kwallet              (Unix-like systems)
-###   keychain             (Mac OS X)
-###   windows-cryptoapi    (Windows)
-password-stores = gnome-keyring,kwallet
-###
-### Set KWallet wallet used by Subversion. If empty or unset,
-### then the default network wallet will be used.
-# kwallet-wallet =
-###
-### Include PID (Process ID) in Subversion application name when
-### using KWallet. It defaults to 'no'.
-# kwallet-svn-application-name-with-pid = yes
-###
-### The rest of this section in this file has been deprecated.
-### Both 'store-passwords' and 'store-auth-creds' can now be
-### specified in the 'servers' file in your config directory.
-### Anything specified in this section is overridden by settings
-### specified in the 'servers' file.
-###
-### Set store-passwords to 'no' to avoid storing passwords in the
-### auth/ area of your config directory.  It defaults to 'yes',
-### but Subversion will never save your password to disk in
-### plaintext unless you tell it to (see the 'servers' file).
-### Note that this option only prevents saving of *new* passwords;
-### it doesn't invalidate existing passwords.  (To do that, remove
-### the cache files by hand as described in the Subversion book.)
-# store-passwords = no
-### Set store-auth-creds to 'no' to avoid storing any subversion
-### credentials in the auth/ area of your config directory.
-### It defaults to 'yes'.  Note that this option only prevents
-### saving of *new* credentials;  it doesn't invalidate existing
-### caches.  (To do that, remove the cache files by hand.)
-# store-auth-creds = no
-
-### Section for configuring external helper applications.
-[helpers]
-### Set editor-cmd to the command used to invoke your text editor.
-###   This will override the environment variables that Subversion
-###   examines by default to find this information ($EDITOR,
-###   et al).
-# editor-cmd = editor (vi, emacs, notepad, etc.)
-### Set diff-cmd to the absolute path of your 'diff' program.
-###   This will override the compile-time default, which is to use
-###   Subversion's internal diff implementation.
-# diff-cmd = diff_program (diff, gdiff, etc.)
-### Set diff3-cmd to the absolute path of your 'diff3' program.
-###   This will override the compile-time default, which is to use
-###   Subversion's internal diff3 implementation.
-# diff3-cmd = diff3_program (diff3, gdiff3, etc.)
-### Set diff3-has-program-arg to 'yes' if your 'diff3' program
-###   accepts the '--diff-program' option.
-# diff3-has-program-arg = [yes | no]
-### Set merge-tool-cmd to the command used to invoke your external
-### merging tool of choice. Subversion will pass 4 arguments to
-### the specified command: base theirs mine merged
-merge-tool-cmd = /usr/bin/svn-merge3-meld
-
-### Section for configuring tunnel agents.
-[tunnels]
-### Configure svn protocol tunnel schemes here.  By default, only
-### the 'ssh' scheme is defined.  You can define other schemes to
-### be used with 'svn+scheme://hostname/path' URLs.  A scheme
-### definition is simply a command, optionally prefixed by an
-### environment variable name which can override the command if it
-### is defined.  The command (or environment variable) may contain
-### arguments, using standard shell quoting for arguments with
-### spaces.  The command will be invoked as:
-###   <command> <hostname> svnserve -t
-### (If the URL includes a username, then the hostname will be
-### passed to the tunnel agent as <user>@<hostname>.)  If the
-### built-in ssh scheme were not predefined, it could be defined
-### as:
-# ssh = $SVN_SSH ssh -q -o ControlMaster=no
-### If you wanted to define a new 'rsh' scheme, to be used with
-### 'svn+rsh:' URLs, you could do so as follows:
-# rsh = rsh
-### Or, if you wanted to specify a full path and arguments:
-# rsh = /path/to/rsh -l myusername
-### On Windows, if you are specifying a full path to a command,
-### use a forward slash (/) or a paired backslash (\\) as the
-### path separator.  A single backslash will be treated as an
-### escape for the following character.
-
-### Section for configuring miscelleneous Subversion options.
-[miscellany]
-### Set global-ignores to a set of whitespace-delimited globs
-### which Subversion will ignore in its 'status' output, and
-### while importing or adding files and directories.
-### '*' matches leading dots, e.g. '*.rej' matches '.foo.rej'.
-global-ignores = *.o *.lo *.la *.al .libs *.so *.so.[0-9]* *.a *.pyc *.pyo *.rej .*.rej *~ .*~ #*# .#* .*.swp *.swp .DS_Store build dist target top ._* .emacs.desktop* .*.marks
-### Set log-encoding to the default encoding for log messages
-# log-encoding = latin1
-### Set use-commit-times to make checkout/update/switch/revert
-### put last-committed timestamps on every file touched.
-# use-commit-times = yes
-### Set no-unlock to prevent 'svn commit' from automatically
-### releasing locks on files.
-# no-unlock = yes
-### Set mime-types-file to a MIME type registry file, used to
-### provide hints to Subversion's MIME type auto-detection
-### algorithm.
-# mime-types-file = /path/to/mime.types
-### Set preserved-conflict-file-exts to a whitespace-delimited
-### list of patterns matching file extensions which should be
-### preserved in generated conflict file names.  By default,
-### conflict files use custom extensions.
-# preserved-conflict-file-exts = doc ppt xls od?
-### Set enable-auto-props to 'yes' to enable automatic properties
-### for 'svn add' and 'svn import', it defaults to 'no'.
-### Automatic properties are defined in the section 'auto-props'.
-enable-auto-props = yes
-### Set interactive-conflicts to 'no' to disable interactive
-### conflict resolution prompting.  It defaults to 'yes'.
-# interactive-conflicts = no
-
-### Section for configuring automatic properties.
-[auto-props]
-### The format of the entries is:
-###   file-name-pattern = propname[=value][;propname[=value]...]
-### The file-name-pattern can contain wildcards (such as '*' and
-### '?').  All entries which match (case-insensitively) will be
-### applied to the file.  Note that auto-props functionality
-### must be enabled, which is typically done by setting the
-### 'enable-auto-props' option.
-*.c = svn:eol-style=native;svn:keywords=Id HeadURL
-*.cpp = svn:eol-style=native;svn:keywords=Id HeadURL
-*.h = svn:eol-style=native;svn:keywords=Id HeadURL
-*.sh = svn:executable;svn:eol-style=native;svn:keywords=Id HeadURL
-*.cmd = svn:mime-type=text/plain;svn:eol-style=CRLF
-*.bat = svn:mime-type=text/plain;svn:eol-style=CRLF
-Makefile* = svn:eol-style=native;svn:keywords=Id HeadURL
-*.obj = svn:mime-type=application/octet-stream
-*.bin = svn:mime-type=application/octet-stream
-*.bmp = svn:mime-type=image/bmp
-*.class = svn:mime-type=application/java
-*.doc = svn:mime-type=application/msword
-*.exe = svn:mime-type=application/octet-stream
-*.gif = svn:mime-type=image/gif
-*.gz = svn:mime-type=application/x-gzip
-*.jar = svn:mime-type=application/java-archive
-*.jelly = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.jpg = svn:mime-type=image/jpeg
-*.jpeg = svn:mime-type=image/jpeg
-*.pdf = svn:mime-type=application/pdf
-*.png = svn:mime-type=image/png
-*.tgz = svn:mime-type=application/octet-stream
-*.tif = svn:mime-type=image/tiff
-*.tiff = svn:mime-type=image/tiff
-*.zip = svn:mime-type=application/zip
-*.txt = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.xml = svn:mime-type=text/xml;svn:eol-style=native;svn:keywords=Id HeadURL
-*.ent = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.dtd = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.vsl = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.xsd = svn:mime-type=text/xml;svn:eol-style=native;svn:keywords=Id HeadURL
-*.xsl = svn:mime-type=text/xml;svn:eol-style=native;svn:keywords=Id HeadURL
-*.wsdl = svn:mime-type=text/xml;svn:eol-style=native;svn:keywords=Id HeadURL
-*.htm = svn:mime-type=text/html;svn:eol-style=native;svn:keywords=Id HeadURL
-*.html = svn:mime-type=text/html;svn:eol-style=native;svn:keywords=Id HeadURL
-*.tmpl = svn:mime-type=text/html;svn:eol-style=native;svn:keywords=Id HeadURL
-*.css = svn:mime-type=text/css;svn:eol-style=native;svn:keywords=Id HeadURL
-*.js = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.jsp = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.txt = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.java = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.properties = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.sql = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-README = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-ReleaseNotes = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.launch = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-.project = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-.classpath = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.rb = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.erb = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.yml = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.rake = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.sample = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.conf = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.rc = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.pl = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.pm = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.t = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.tt = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.launch = svn:mime-type=text/plain;svn:eol-style=native;svn:keywords=Id HeadURL
-*.diff = svn:eol-style=native;svn:keywords=Id HeadURL

.subversion/svn-merge3-meld.sh
@@ -1,40 +1,0 @@
-#!/bin/sh
-#
-# NAME
-#        svn-merge3-meld.sh - Meld wrapper for Subversion 3-way merge
-#
-# SYNOPSIS
-#        svn-merge3-meld.sh BASE THEIRS MINE MERGED [PARTIAL]
-#
-# DESCRIPTION
-#        This rearranges and prunes the parameters from Subversion to fit the
-#        Meld synopsis. PARTIAL is not used by Meld, but is sent by
-#        Subversion.
-#
-# INSTALL
-#        Global install:
-#        1. Make this script available in a global location:
-#               sudo ln -s /path/to/svn-merge3-meld.sh /usr/bin/svn-merge3-meld
-#        2. Add the following line in ~/.subversion/config:
-#               merge-tool-cmd = /usr/bin/svn-merge3-meld
-#
-#        Single user install:
-#        1. Add the following line in ~/.subversion/config (The path must be
-#           absolute):
-#               merge-tool-cmd = /path/to/svn-merge3-meld.sh
-#
-# BUGS
-#        https://github.com/l0b0/tilde/issues
-#
-# AUTHOR
-#        Written by Victor Engmark
-#
-# COPYRIGHT
-#        Copyright © 2011 Victor Engmark. License GPLv3+: GNU GPL
-#        version 3 or later <http://gnu.org/licenses/gpl.html>.
-#        This is free software: you are free to change and redistribute it.
-#        There is NO WARRANTY, to the extent permitted by law.
-#
-################################################################################
-
-meld "$3" "$1" "$2" "$4"

Makefile
@@ -12,7 +12,6 @@ DOTFILES = $(wildcard .*rc) \
            .screenlayout \
            .screenrc \
            .signatures \
-           .subversion \
            .vim \
            .xinitrc \
            .Xresources

README.markdown
@@ -8,7 +8,6 @@ These will probably be moved to separate projects after a while:
 * `tools.mk` - Auxiliary `make` targets.
 * `scripts/duplicates.sh` - Check for duplicate files in two directories.
 * `scripts/make-links.sh` - Create symbolic links for several files in a separate directory, asking for confirmation or diffing if there is a collision.
-* `scripts/__svn_ps1.sh` - Similar to [`__git_ps1.sh`](), provides a string usable in Bash's `$PS1` when sourced while inside a Subversion repository.
 
 Install
 -------

scripts/__svn_ps1.sh
@@ -1,59 +1,0 @@
-#!/bin/sh
-#
-# NAME
-#        __svn_ps1.sh - Get the current Subversion branch, tag or "trunk"
-#
-# SYNOPSIS
-#        __svn_ps1.sh [FORMAT]
-#
-# DESCRIPTION
-#        For use with the PS1 or other PS variables in Bash.
-#
-#        The FORMAT string at the end is optional. If present, it *must* contain
-#        a valid printf format string including a '%s'. Default ' (%s)'.
-#
-#        Based on the awesome __git_ps1.
-#
-# INSTALLATION
-#        Add the following line to ~/.bashrc:
-#        PS1="${PS1}\$(__svn_ps1 ' (%s)')"
-#
-#        To enable for all users who have bash_completion:
-#        $ sudo mv __svn_ps1.sh /etc/bash_completion.d/
-#
-#        If that doesn't work, or you prefer a single user installation, add
-#        also the following line to ~/.bashrc:
-#        source /path/to/__svn_ps1.sh
-#
-# COPYRIGHT
-#        Copyright © 2010-2011 Victor Engmark. License GPLv3+: GNU GPL
-#        version 3 or later <http://gnu.org/licenses/gpl.html>.
-#        This is free software: you are free to change and redistribute it.
-#        There is NO WARRANTY, to the extent permitted by law.
-#
-################################################################################
-
-# Explanation:
-# `svn info` gets the branch and other information
-# `sed -r` allows extended regular expressions
-# If in trunk, just output "trunk". If anywhere else, output the next part of the path
-#
-# Tests:
-# $ echo 'URL: http://example.org/svn/branches/SUCCESS/FAIL' | perl -ne 'print if s;^URL: .*?/((trunk)|(branches|tags)/([^/]*)).*;\2\4 ;'
-# SUCCESS
-# $ echo 'URL: http://example.org/svn/tags/SUCCESS/FAIL' | perl -ne 'print if s;^URL: .*?/((trunk)|(branches|tags)/([^/]*)).*;\2\4 ;'
-# SUCCESS
-# $ echo 'URL: http://example.org/svn/trunk/FAIL' | perl -ne 'print if s;^URL: .*?/((trunk)|(branches|tags)/([^/]*)).*;\2\4 ;'
-# trunk
-# $ echo 'URL: http://example.org/svn/tags/SUCCESS/tags/FAIL' | perl -ne 'print if s;^URL: .*?/((trunk)|(branches|tags)/([^/]*)).*;\2\4 ;'
-# SUCCESS
-
-__svn_ps1()
-{
-    test -d '.svn' || return
-    local result=$(
-        svn info 2>/dev/null | \
-        perl -ne 'print if s;^URL: .*?/((trunk)|(branches|tags)/([^/]*)).*;\2\4 ;')
-    test -n "$result" || return
-    printf "${1:- (%s)}" $result
-}

commit d1bff90011b712cd9c00f6402b993c6ccbf62541

Fix prompt end
Regression from fef5a4701fcece77895ca9ba29129ab73a8f8207.

.bashrc
@@ -132,6 +132,9 @@ then
     export GIT_PS1_SHOWUPSTREAM="auto"
 fi
 
+# Prompt end
+PS1="$PS1"$'\n$ '
+
 # Default editor
 export GIT_EDITOR='vim'
 export VISUAL='vim'