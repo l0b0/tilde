dd241e1619b817cfbe9e386bae21413dfdd398ad
Victor Engmark <victor.engmark@gmail.com>
2015-10-29 21:44:49 +0000

    Use `terminal` variable to run the configured terminal command

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -514,7 +514,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- {{{ Autostart
 -- Start most common applications
 awful.util.spawn("firefox")
-awful.util.spawn("xterm")
+awful.util.spawn(terminal)
 -- }}}
 
 -- Local configuration
