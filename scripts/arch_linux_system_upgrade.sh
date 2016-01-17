e17aac92a8504dbe0e6739cdcd72a00a85fd8781
Victor Engmark <victor.engmark@gmail.com>
2016-01-17 12:50:38 +0000

    Fix CUPS not printing after update
    See
    <http://www.craiglotter.co.za/2013/01/12/solved-print-error-ppd-version-is-not-compatible-with-gutenprint/>.

--- scripts/arch_linux_system_upgrade.sh
+++ scripts/arch_linux_system_upgrade.sh
@@ -4,6 +4,12 @@ set -o errexit -o noclobber -o nounset
 
 sudo pacman --sync --refresh --sysupgrade
 
+# Fix CUPS PPD files
+if sudo /usr/sbin/cups-genppdupdate | grep Updated
+then
+    sudo systemctl restart org.cups.cupsd
+fi
+
 find "${HOME}/dev/tilde/patches" -type f -exec sudo patch --directory=/ --forward --strip=0 --input={} ';'
 
 running_kernel="$(uname -r)"
