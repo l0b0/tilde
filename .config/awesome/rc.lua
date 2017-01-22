9109c2af62c1ac6147d55a81f5b090fafafc2f6e
Victor Engmark <victor.engmark@gmail.com>
2017-01-22 12:37:38 +0000

    Use vim as the default editor for awesome configuration

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -42,7 +42,7 @@ beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
