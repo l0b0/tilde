acf72d6320255f3fbf9fcc68c6d8d0787d522b40
Victor Engmark <victorengmark@catalyst.net.nz>
2018-01-08 09:25:53 +1300

    Deprioritise pacnew detection
    Also invert exit code to make sure the script is successful if there are no
    files to merge.

--- scripts/arch_linux_system_upgrade.sh
+++ scripts/arch_linux_system_upgrade.sh
@@ -16,12 +16,12 @@ fi
 
 find "$(dirname "$directory")/patches" -type f -exec patch --directory=/ --forward --strip=0 --input={} ';'
 
-updatedb
-locate .pacnew
-
 running_kernel="$(uname -r)"
 installed_kernel="$(file --brief /boot/vmlinuz-linux | sed 's/.* version \([^ ]\+\).*/\1/')"
 if [ "$running_kernel" != "$installed_kernel" ]
 then
     printf "Reboot needed to update running kernel from %s to %s\n" "$running_kernel" "$installed_kernel" >&2
 fi
+
+updatedb
+! locate .pacnew
