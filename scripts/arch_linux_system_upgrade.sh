9bc6425acf763f892bda648a46d39857e4940953
Victor Engmark <victorengmark@catalyst.net.nz>
2018-01-19 09:25:38 +1300

    Use bash test syntax

--- scripts/arch_linux_system_upgrade.sh
+++ scripts/arch_linux_system_upgrade.sh
@@ -19,7 +19,7 @@ find "$(dirname "$directory")/patches" -type f -exec patch --directory=/ --forwa
 
 running_kernel="$(uname -r)"
 installed_kernel="$(file --brief /boot/vmlinuz-linux | sed 's/.* version \([^ ]\+\).*/\1/')"
-if [ "$running_kernel" != "$installed_kernel" ]
+if [[ "$running_kernel" != "$installed_kernel" ]]
 then
     printf "Reboot needed to update running kernel from %s to %s\n" "$running_kernel" "$installed_kernel" >&2
 fi
